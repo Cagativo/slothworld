99453b53894fd766a41e6f11fc95e07f74310a16
Cagativo <andre.domingues9@gmail.com>
2026-05-07 21:28:38 +0100

    chore(core): remove disabled trend research bridge shim

--- core/trendResearchAgentBridge.js
+++
@@ -1,6 +0,0 @@
-// TrendResearch bridge intentionally disabled.
-// Trend panel state is derived by selectors from indexed events.
-
-export function initTrendResearchAgentReactions() {
-  // No-op by design.
-}
--- ui/ui-bootstrap.js
+++ ui/ui-bootstrap.js
@@ -1,13 +1,9 @@
 import { initOperatorControlPanel } from './operator-control-panel.js';
 import { initRaccoonFeederPanel } from './raccoon-feeder-panel.js';
 import { initTaskCreatorPanel } from './task-creator-panel.js';
-import { initTrendResearchAgentReactions } from '../core/trendResearchAgentBridge.js';
-import { agents } from '../core/app-state.js';
 
 export function initUI() {
   initOperatorControlPanel();
   initRaccoonFeederPanel();
   initTaskCreatorPanel();
-  initTrendResearchAgentReactions(agents);
-  console.log('[bootstrap] trendResearchAgentReactions initialised');
 }
